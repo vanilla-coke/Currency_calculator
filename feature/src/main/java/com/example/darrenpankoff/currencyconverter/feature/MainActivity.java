f6be58afd831a013c712409367dd067b342bc960
Darren Pankoff <dpankoff@uwo.ca>
2018-05-26 20:23:00 -0400

    second commit, added spinner and API connections

--- feature/src/main/java/com/example/darrenpankoff/currencyconverter/feature/MainActivity.java
+++ feature/src/main/java/com/example/darrenpankoff/currencyconverter/feature/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.darrenpankoff.currencyconverter.feature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.example.darrenpankoff.currencyconverter.feature.Data.Countries;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private double selected_currency = 0.0;
     private double second_currency = 0.0;
     private double third_currency = 0.0;
+
     private boolean decimalClicked = false;
 
     private String total_currency1 = "";
@@ -30,161 +34,155 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Countries api_access = new Countries(); // use this to make async calls
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.currency_spinner1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.currency_spinner2);
+        Spinner spinner3 = (Spinner) findViewById(R.id.currency_spinner3);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countryNames, android.R.layout.simple_list_item_1);
+        spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner3.setAdapter(adapter);
+
     }
 
 
 
 
+    // resource ID's cannot be used in a switch statement therefore needed to use if else
+
     public void CalcButtonClick(View v){
-        switch(v.getId()){
-            case R.id.num0: {
-                if(selected_currency == 0){
-                    break;
-                }
-                if(decimalClicked){
-                    break;
-                }
-                else{
-                    selected_currency *= 10;
-                    display();
-
-                }
+        int result = v.getId();
+        if(result == R.id.num0){
+            if(selected_currency != 0 && !decimalClicked){
+                selected_currency *= 10;
+                display();
             }
-            case R.id.num1: {
-                if(decimalClicked){
-                    total_currency1 += "1";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
+        }
+        else if (result == R.id.num1){
+            if(decimalClicked){
+                total_currency1 += "1";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-                }else{
-                    selected_currency++;
-                    display();
-
-                }
+            }else{
+                selected_currency++;
+                display();
 
             }
-            case R.id.num2: {
-                if(decimalClicked){
-                    total_currency1 += "2";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
+        }
+        else if (result == R.id.num2){
+            if(decimalClicked){
+                total_currency1 += "2";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-                }else{
-                    selected_currency++;
-                    display();
-
-                }
-
-            }
-            case R.id.num3: {
-                if(decimalClicked){
-                    total_currency1 += "3";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
-
-                }else{
-                    selected_currency++;
-                    display();
-
-                }
+            }else{
+                selected_currency++;
+                display();
 
             }
-            case R.id.num4: {
-                if(decimalClicked){
-                    total_currency1 += "4";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
+        }
+        else if (result == R.id.num3){
+            if(decimalClicked){
+                total_currency1 += "3";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-                }else{
-                    selected_currency++;
-                    display();
-
-                }
-
-            }
-            case R.id.num5: {
-                if(decimalClicked){
-                    total_currency1 += "5";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
-
-                }else{
-                    selected_currency++;
-                    display();
-
-                }
+            }else{
+                selected_currency++;
+                display();
 
             }
-            case R.id.num6: {
-                if(decimalClicked){
-                    total_currency1 += "6";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
+        }
+        else if (result == R.id.num4){
+            if(decimalClicked){
+                total_currency1 += "4";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-                }else{
-                    selected_currency++;
-                    display();
-
-                }
+            }else{
+                selected_currency++;
+                display();
 
             }
-            case R.id.num7: {
-                if(decimalClicked){
-                    total_currency1 += "7";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
+        }
+        else if (result == R.id.num5){
+            if(decimalClicked){
+                total_currency1 += "5";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-                }else{
-                    selected_currency++;
-                    display();
+            }else{
+                selected_currency++;
+                display();
 
-                }
+            }
+        }
+        else if (result == R.id.num6){
+            if(decimalClicked){
+                total_currency1 += "6";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-            }case R.id.num8: {
-                if(decimalClicked){
-                    total_currency1 += "8";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
+            }else{
+                selected_currency++;
+                display();
 
-                }else{
-                    selected_currency++;
-                    display();
+            }
+        }
+        else if (result == R.id.num7){
+            if(decimalClicked){
+                total_currency1 += "7";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-                }
+            }else{
+                selected_currency++;
+                display();
 
-            }case R.id.num9: {
-                if(decimalClicked){
-                    total_currency1 += "9";
-                    selected_currency = Double.parseDouble(total_currency1);
-                    display();
+            }
+        }
+        else if (result == R.id.num8){
+            if(decimalClicked){
+                total_currency1 += "8";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-                }else{
-                    selected_currency++;
-                    display();
+            }else{
+                selected_currency++;
+                display();
 
-                }
+            }
+        }
+        else if (result == R.id.num9){
+            if(decimalClicked){
+                total_currency1 += "9";
+                selected_currency = Double.parseDouble(total_currency1);
+                display();
 
-            }case R.id.decimal: {
-                decimalClicked = !decimalClicked;
+            }else{
+                selected_currency++;
+                display();
 
-            }case R.id.clear: {
-                selected_currency = 0.0;
-                second_currency = 0.0;
-                third_currency = 0.0;
-                total_currency1 = "";
-                total_currency2 = "";
-                total_currency3 = "";
-
-            }case R.id.deletePrev: {
-                if(selected_currency == 0.0){
-                    break;
-                }
-                else{
-                    total_currency1 = total_currency1.substring(0, total_currency1.length()-1);
-                    selected_currency = Double.parseDouble(total_currency1);
-
-                }
-
+            }
+        }else if (result == R.id.decimal) {
+            decimalClicked = !decimalClicked;
+        }
+        else if(result == R.id.clear) {
+            selected_currency = 0.0;
+            second_currency = 0.0;
+            third_currency = 0.0;
+            total_currency1 = "";
+            total_currency2 = "";
+            total_currency3 = "";
+        }
+        else if(result == R.id.deletePrev){
+            if(selected_currency != 0.0){
+                total_currency1 = total_currency1.substring(0, total_currency1.length()-1);
+                selected_currency = Double.parseDouble(total_currency1);
             }
         }
     }
@@ -201,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
         total_currency3 = String.format("%.3f", third_currency);
 
 
-        TextView current = (TextView) findViewById(R.id.first_currency);
+        TextView current = (TextView)findViewById(R.id.first_currency);
         current.setText(total_currency1);
 
 
